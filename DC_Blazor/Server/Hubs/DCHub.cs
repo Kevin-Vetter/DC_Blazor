9dfeb91e02aade1181ad247913957c1502d8267d
Kevin Vetter <kevinmvetter@gmail.com>
2023-03-06 15:21:01 +0100

    Add project files.

---
+++ DC_Blazor/Server/Hubs/DCHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Net.Mail;
+using System.Runtime.CompilerServices;
+
+namespace DC_Blazor.Server.Hubs
+{
+    public class DCHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            UserHandler.ConnectedIds.Add(Context.ConnectionId);
+
+            await Sendmsg($"User Joined, there are {UserHandler.ConnectedIds.Count} users here now!");
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+            await Sendmsg($"User left, there are {UserHandler.ConnectedIds.Count} users here now!");
+            await base.OnDisconnectedAsync(exception);
+        }
+        
+        public async Task Sendmsg(string msg)
+        {
+            await Clients.All.SendAsync("ReceiveMsg", msg);
+        }
+    }
+}
---
+++ DC_Blazor/Server/Hubs/UserHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace DC_Blazor.Server.Hubs
+{
+    public static class UserHandler
+    {
+        public static List<string> ConnectedIds = new List<string>();
+    }
+}
